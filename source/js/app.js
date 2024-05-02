9523d7ae5769fb8a72a252b56556264d98551035
Maria Lisitsa <sokalslash@mail.ru>
2024-05-03 00:48:54 +0300

    feat: add adaptive header

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const navElement = document.querySelector('.page-header__nav');
-const burgerButton = document.querySelector('.page-header__toggle')
+const burgerButtonElement = document.querySelector('.page-header__toggle')
 
 console.log(burgerButton);
 
 navElement.classList.remove('page-header__nav--opend');
 navElement.classList.add('page-header__nav--closed');
 
-burgerButton.addEventListener('click', () => {
+burgerButtonElement.addEventListener('click', () => {
   if (navElement.classList.contains('page-header__nav--opend')) {
     navElement.classList.remove('page-header__nav--opend');
     navElement.classList.add('page-header__nav--closed');
